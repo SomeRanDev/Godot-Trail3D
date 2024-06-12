e2b9a10914bdb548087b08371ef0d3aa9ecebf6f
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2024-06-12 13:33:37 -0400

    Fixed BUILD.sh to move .so files instead of .dll

--- BUILD.sh
+++ BUILD.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cargo build
 cargo build --release
-mv target/debug/godot_trail_3d.dll addons/SomeRanDev_Trail3D/godot_trail_3d_debug.dll
-mv target/release/godot_trail_3d.dll addons/SomeRanDev_Trail3D/godot_trail_3d.dll
+mv target/debug/godot_trail_3d.so addons/SomeRanDev_Trail3D/godot_trail_3d_debug.so
+mv target/release/godot_trail_3d.so addons/SomeRanDev_Trail3D/godot_trail_3d.so
